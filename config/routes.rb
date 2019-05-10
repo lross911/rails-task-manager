490fde550d21990a5134ea99a94b1b4d0cfe4f93
Lavert Ross <lross911@gmail.com>
2019-05-10 16:50:59 +0100

    Task Challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  post 'tasks', to: 'tasks#create'
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'task#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
+
+# route , action (controller) , view (show)
